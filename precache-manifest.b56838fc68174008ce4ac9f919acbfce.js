4cb396c451e1ba8d58408120b2ad3ada3c8cdbc7
Nazarii Lavrenchuk <jackil182@gmail.com>
2019-03-15 19:15:43 +0200

    Updates

---
+++ precache-manifest.b56838fc68174008ce4ac9f919acbfce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57609903018f84bbd8f0",
+    "url": "/weight-tracker-app/static/js/runtime~main.ee4e98d7.js"
+  },
+  {
+    "revision": "315b2a61b087c1da2862",
+    "url": "/weight-tracker-app/static/js/main.e8b0fa7c.chunk.js"
+  },
+  {
+    "revision": "201fab2f69e247c0802c",
+    "url": "/weight-tracker-app/static/js/2.9fd5f3e3.chunk.js"
+  },
+  {
+    "revision": "315b2a61b087c1da2862",
+    "url": "/weight-tracker-app/static/css/main.6bb29cdb.chunk.css"
+  },
+  {
+    "revision": "201fab2f69e247c0802c",
+    "url": "/weight-tracker-app/static/css/2.3fde9c12.chunk.css"
+  },
+  {
+    "revision": "f0337d4cda7efc10041c6a8b3900de40",
+    "url": "/weight-tracker-app/index.html"
+  }
+];
